8dec8ed6d81dc3a1e89c519af1c159ed46c49ce9
James Junior <jamescosjr@gmail.com>
2024-10-16 21:17:02 -0300

    feat: add productRepository tests for CRUD functions and utility functions

---
+++ src/__tests__/repository/productRepository.test.js
@@ -0,0 +1,121 @@
+import { createProduct, listProducts, findProductById, findProductByName } from "../../repository/productRepository.js";
+import { generateId } from "../../utils/idGenerator.js";
+import { products } from "../../repository/productRepository.js";
+
+jest.mock("../../utils/idGenerator.js");
+const mockGenerateId = generateId;
+
+describe("ProductManagementRepository", () => {
+  beforeEach(() => {
+    products.length = 0;
+    generateId.mockClear();
+  });
+  it("should create a product and add to the array", () => {
+    const product = {
+      name: "Product 1",
+      category: "Category 1",
+      price: 100,
+      stock: 10,
+    };
+    const id = generateId();
+    mockGenerateId.mockReturnValue(id);
+    const expectedProduct = {
+      id,
+      name: "Product 1",
+      category: "Category 1",
+      price: 100,
+      stock: 10,
+    };
+    createProduct(product);
+    expect(products).toHaveLength(1);
+    expect(products[0]).toEqual(expectedProduct);
+  });
+
+  it("should use the method listProducts correctly", () => {
+    const product = {
+      name: "Product 1",
+      category: "Category 1",
+      price: 100,
+      stock: 10,
+    };
+    const id = generateId();
+    mockGenerateId.mockReturnValue(id);
+    createProduct(product);
+    const product2 = {
+      name: "Product 2",
+      category: "Category 2",
+      price: 200,
+      stock: 20,
+    };
+    const id2 = generateId();
+    mockGenerateId.mockReturnValue(id2);
+    createProduct(product2);
+    const expectedProducts = [
+      {
+        id,
+        name: "Product 1",
+        category: "Category 1",
+        price: 100,
+        stock: 10,
+      },
+      {
+        id: id2,
+        name: "Product 2",
+        category: "Category 2",
+        price: 200,
+        stock: 20,
+      },
+    ];
+    expect(listProducts()).toEqual(expectedProducts);
+  });
+
+    it ("should return an empty array when there are no products", () => {
+        expect(listProducts()).toEqual([]);
+    });
+
+    it ("should return the product with the given id", () => {
+        const product = {
+            name: "Product 1",
+            category: "Category 1",
+            price: 100,
+            stock: 10,
+        };
+        const id = generateId();
+        mockGenerateId.mockReturnValue(id);
+        createProduct(product);
+
+       const  expectedProduct = {
+            id,
+            name: "Product 1",
+            category: "Category 1",
+            price: 100,
+            stock: 10,
+        };
+        expect(findProductById(id)).toEqual(expectedProduct);
+    });
+
+    it ("should return undefined when the product with the given id does not exist", () => {
+        expect(findProductById("123")).toBeUndefined();
+    });
+
+    it ("should return the product with the given name", () => {
+        const product = {
+            name: "Product 1",
+            category: "Category 1",
+            price: 100,
+            stock: 10,
+        };
+        const id = generateId();
+        mockGenerateId.mockReturnValue(id);
+        createProduct(product);
+
+        const expectedProduct = {
+            id,
+            name: "Product 1",
+            category: "Category 1",
+            price: 100,
+            stock: 10,
+        };
+        expect(findProductByName("Product 1")).toEqual(expectedProduct);
+    });
+});
