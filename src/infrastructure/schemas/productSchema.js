b9053dab0162fa10b9f31ecf8e7b0806106e99f4
jamescosjr <jamescosjr@gmail.com>
2025-01-06 19:59:42 -0300

    feat: add product schema for MongoDB using Mongoose

---
+++ src/infrastructure/schemas/productSchema.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+export const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    stock: {
+        type: Number,
+        required: true,
+    },
+});
+
+export const Product = mongoose.model("Product", productSchema);
